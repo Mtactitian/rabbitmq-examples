416a64088b54df22c56c59ad4a37457dac6bd2f0
Alex B <brinza.alexey@gmail.com>
2018-08-09 21:08:14 +0300

    configure postgre profile

--- emp-consumer/src/main/java/com/alexb/empconsumer/configuration/RabbitConfiguration.java
+++ emp-consumer/src/main/java/com/alexb/empconsumer/configuration/RabbitConfiguration.java
@@ -4,13 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.RabbitListenerErrorHandler;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.exception.ListenerExecutionFailedException;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,17 +18,6 @@ public class RabbitConfiguration {
     @Bean
     public MessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
-    }
-
-    @Bean
-    public SimpleMessageListenerContainer listenerContainer(ConnectionFactory factory,
-                                                            @Value("${alexb.queue.name}") String queueName) {
-        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-        container.setConnectionFactory(factory);
-        container.setQueueNames(queueName);
-        container.setMessageConverter(messageConverter());
-
-        return container;
     }
 
     @Bean
