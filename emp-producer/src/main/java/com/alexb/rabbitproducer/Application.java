ac787dbaf0413e05c60477fed6e4235b3c0daf4f
Alex B <brinza.alexey@gmail.com>
2018-08-13 21:04:43 +0300

    add spring cloud config

--- emp-producer/src/main/java/com/alexb/rabbitproducer/Application.java
+++ emp-producer/src/main/java/com/alexb/rabbitproducer/Application.java
@@ -1,13 +1,24 @@
 package com.alexb.rabbitproducer;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+@Slf4j
+public class Application implements CommandLineRunner {
+
+    @Value("${app.name: Undefined}")
+    private String applicationName;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Override
+    public void run(String... args) {
+        log.info("Application name: " + applicationName);
+    }
 }
